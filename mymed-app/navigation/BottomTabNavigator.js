7593f5edc26dddcd998147846ac752a17c8ef8b8
Samantha Lincroft <petraarkian.heg.gov@gmail.com>
2020-06-18 19:36:01 +1000

    Add bare bones expo app for development.

---
+++ mymed-app/navigation/BottomTabNavigator.js
@@ -0,0 +1,59 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import * as React from 'react';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinkScreen from '../screens/LinkScreen';
+import FileScreen from '../screens/FileScreen';
+
+const BottomTab = createBottomTabNavigator();
+const INITIAL_ROUTE_NAME = 'Home';
+
+export default function BottomTabNavigator({ navigation, route }) {
+  // Set the header title on the parent stack navigator depending on the
+  // currently active tab. Learn more in the documentation:
+  // https://reactnavigation.org/docs/en/screen-options-resolution.html
+  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+
+  return (
+    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-home" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Files"
+        component={FileScreen}
+        options={{
+          title: 'Files',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-folder" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Links"
+        component={LinkScreen}
+        options={{
+          title: 'Help',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+function getHeaderTitle(route) {
+  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+
+  switch (routeName) {
+    case 'Home':
+      return 'Welcome to MyMed';
+    case 'Links':
+      return 'Help';
+    case 'Files':
+      return 'Files';
+  }
+}
